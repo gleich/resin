63d2eb44bb52c4d4e96606bfa640056ca58775b0
Matthew Gleich <git@mattglei.ch>
2021-04-29 17:29:09 -0400

    feat: add unit test for cli
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,11 @@
 use std::process;
 
-use anyhow::Result;
-
+#[derive(Debug, PartialEq)]
 pub struct Args {
 	pub verbose: bool,
 }
 
-pub fn parse_args(args: Vec<String>) -> Result<Args> {
+pub fn parse_args(args: Vec<String>) -> Args {
 	if has_flag(&args, "help", "h") {
 		println!(
 			"stabit :: cli interface for conventional commits
@@ -18,11 +17,34 @@ flags:
 		process::exit(0);
 	}
 
-	Ok(Args {
+	Args {
 		verbose: has_flag(&args, "verbose", "v"),
-	})
+	}
 }
 
 fn has_flag(args: &Vec<String>, name: &str, shorthand: &str) -> bool {
 	args.contains(&format!("--{}", name)) || args.contains(&format!("-{}", shorthand))
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::utils::to_string_vec;
+
+	#[test]
+	fn test_parse_args() {
+		assert_eq!(parse_args(to_string_vec(vec![""])), Args { verbose: false });
+		assert_eq!(
+			parse_args(to_string_vec(vec!["-v"])),
+			Args { verbose: true }
+		);
+		assert_eq!(
+			parse_args(to_string_vec(vec!["--verbose"])),
+			Args { verbose: true }
+		);
+		assert_eq!(
+			parse_args(to_string_vec(vec!["random", "content"])),
+			Args { verbose: false }
+		);
+	}
 }
