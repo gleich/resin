94d81ec5cb9e974140dd989f28d14df9a15fd340
Matthew Gleich <git@mattglei.ch>
2021-05-08 19:40:43 -0400

    revert(remove): get rid of goreleaser for release system

--- release.sh
+++
@@ -1,9 +0,0 @@
-# Building binaries
-cargo build --release --target=x86_64-unknown-linux-gnu
-cross build --release --target=x86_64-apple-darwin
-cross build --release --target=x86_64-pc-windows-gnu
-
-# Moving binaries
-mkdir -p dist/darwin_amd64 && cp target/x86_64-apple-darwin/release/resin dist/darwin_amd64
-mkdir -p dist/linux_amd64 && cp target/x86_64-unknown-linux-gnu/release/resin dist/linux_amd64
-mkdir -p dist/windows_amd64 && cp target/x86_64-pc-windows-gnu/release/resin.exe dist/windows_amd64
