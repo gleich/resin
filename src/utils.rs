f39d5999fbb88918b8fd3150c96a8dec8e004a23
Matt Gleich <git@mattglei.ch>
2021-10-24 22:31:21 -0400

    fix(lint): cargo fmt
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,16 @@
 use colored::Colorize;
 
-pub fn to_string_vec(vec: Vec<&str>) -> Vec<String> { vec.into_iter().map(String::from).collect() }
+pub fn to_string_vec(vec: Vec<&str>) -> Vec<String> {
+	vec.into_iter().map(String::from).collect()
+}
 
-pub fn output_success(message: &str) { println!("{} {}", "✔".green(), message.bold()) }
+pub fn output_success(message: &str) {
+	println!("{} {}", "✔".green(), message.bold())
+}
 
-pub fn output_failure(message: &str) { println!("{} {}", "✗".red(), message.bold()) }
+pub fn output_failure(message: &str) {
+	println!("{} {}", "✗".red(), message.bold())
+}
 
 #[cfg(test)]
 mod tests {
